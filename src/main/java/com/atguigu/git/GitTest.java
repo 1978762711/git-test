1dda8812bb481cd3adeed3cb12b04cc39c680d31
Layne <Layne@atguigu.com>
2021-06-23 13:30:24 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author name
+ * @date 2021/6/23 13:27
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
